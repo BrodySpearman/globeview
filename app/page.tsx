1395a4e3848c4952eb35dfdd8630abcf999eb510
Brody Spearman <brodyspearman@gmail.com>
2026-02-03 03:50:49 -0600

    Added clock module and better header.

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 import Image from "next/image";
 import dynamic from "next/dynamic";
+import Logo from "./Components/Logo/Logo";
 
 const WorldMapNoSSR = dynamic(() => import("./Components/WorldMap/WorldMap"), {
   ssr: false,
@@ -8,9 +9,9 @@ const WorldMapNoSSR = dynamic(() => import("./Components/WorldMap/WorldMap"), {
 
 export default function Home() {
   return (
-      <main className="flex min-h-screen flex-col items-center justify-between">
-        <h1 className="text-3xl fixed m-auto z-100 pl-2 mt-2 mb-2 pr-2 bg-black">Map Test</h1>
-        <WorldMapNoSSR />
-      </main>
+    <main className="flex min-h-screen flex-col items-center justify-between">
+      <Logo />
+      <WorldMapNoSSR />
+    </main>
   );
 }
