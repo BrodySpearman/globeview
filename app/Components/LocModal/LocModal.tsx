a34cd0d19fe142c2ebfa10854b46016d24fc3edd
Brody Spearman <brodyspearman@gmail.com>
2026-02-03 05:44:59 -0600

    Modal branch creation commit

--- app/Components/LocModal/LocModal.tsx
+++ app/Components/LocModal/LocModal.tsx
@@ -1,12 +1,24 @@
 import { Coordinate } from 'ol/coordinate';
 import styles from './LocModal.module.css';
 
-export default function LocModal({ coordinates }: { coordinates: Coordinate }) { 
+
+
+export default function LocModal({ coordinates }: { coordinates: Coordinate }) {
+
+    let lat = coordinates[1];
+    let lon = coordinates[0];
+
+    let latDir = lat >= 0 ? 'N' : 'S';
+    let lonDir = lon >= 0 ? 'E' : 'W';
+
+    lat = Math.abs(lat);
+    lon = Math.abs(lon);
+
     return (
         <div className={styles.modal}>
             <h2 className={styles.title}>You are at:</h2>
-            <p className={styles.lat}>Longitude: {coordinates[0].toFixed(6)}</p>
-            <p className={styles.lon}>Latitude: {coordinates[1].toFixed(6)}</p>
+            <p className={styles.lon}>Longitude: {lon.toFixed(4)}° {lonDir}</p>
+            <p className={styles.lat}>Latitude: {lat.toFixed(4)}° {latDir}</p>
         </div>
     );
 }
