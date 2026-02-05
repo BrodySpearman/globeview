4f4fa3ea9240121ec6ca207a5bcf40df9e33709f
Brody Spearman <brodyspearman@gmail.com>
2026-02-05 00:12:38 -0600

    Fixed merge conflict

--- app/Components/LocModal/LocModal.tsx
+++ app/Components/LocModal/LocModal.tsx
@@ -22,8 +22,6 @@ export default function LocModal({ coordinates }: { coordinates: Coordinate }) {
             <h2 className={styles.title}>You are at:</h2>
             <p className={styles.lat}>Latitude: {lat.toFixed(4)}° {latDir}</p>
             <p className={styles.lon}>Longitude: {lon.toFixed(4)}° {lonDir}</p>
-            <p className={styles.lon}>Longitude: {lon.toFixed(4)}° {lonDir}</p>
-            <p className={styles.lat}>Latitude: {lat.toFixed(4)}° {latDir}</p>
         </div>
     );
 }
